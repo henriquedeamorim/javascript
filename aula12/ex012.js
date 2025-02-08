1b0493e2568747f2d6ed19a6c740c5a26568f613
Henrique de Amorim <henriquedeamorim@outlook.com>
2025-02-08 17:00:21 -0300

    if else

---
+++ aula12/ex012.js
@@ -0,0 +1,11 @@
+
+var hora = 15
+  console.log(`Agora são exatamente ${hora} horas.`)
+if (hora > 6 && hora < 11)
+  console.log(`Bom dia!`)
+else if (hora > 12 && hora < 17)
+  console.log(`Boa Tarde!`)
+else if (hora > 18 && hora < 24)
+  console.log(`Boa Noite!`)
+else (hora > 1 && hora < 5)
+  console.log(`boa Madrugada!`)
