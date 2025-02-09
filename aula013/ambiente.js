6663f6a381d2e55f8358b8b44e5671b1e3470786
Henrique de Amorim <henriquedeamorim@outlook.com>
2025-02-09 11:35:06 -0300

    Sistema de Repetições
    introdução - while[enquanto] do[faça]

---
+++ aula013/ambiente.js
@@ -0,0 +1,25 @@
+var c = 1;
+do {
+  console.log(`Passo ${c}`);
+  c++;
+} while (c <= 6);
+
+// >>>>>>>>>>  while = ENQUANTO <<<<<<<<<
+// >>>>>>>>>>>    do = FAÇA   <<<<<<<<<<< 
+
+/*
+var c = 1
+while (c <= 10) { 
+  console.log(`Passo ${c}`)
+  c++ // c = c + 1 
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
